707399e35abd0e993fa83ea7207b3404c1805a90
Nefito <43294012+Nefito@users.noreply.github.com>
2018-10-13 13:54:34 +0300

    Update lab2(code).c

--- lab2(code).c
+++ lab2(code).c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
+// here I make a factorial function
+double fac(double n)
+    {
+        if (n == 0 || n == 1) return 1;
+        return n * fac(n - 1);
+    }
+
+
 int main()
 {
-    double n, a, s, x;
+    double n, a, x, A, s;
     n = 1;
     s = 0;
-    a = 1;
-    
+
     do
-    {    
-        x = 1 / ((3*n + 1)*(3*n + 2));
-        s += a;
-        a = x*( 1 / ((3*(n-1) + 2)*(3*(n-1) + 1)));
+    {
+        a = (pow(3, n)*fac(n)) / fac((3*n));\
+        // x is the reccurent formula from a
+        x = 1/((3*n + 1)*(3*n + 2));
+        A = a*x;
+        s = A + s;
         n++;
-    }while (a > 0.0001);
-    
-    printf("the sum is %lf!\n", s);
-    
+    }while(A > 0.000l);
+    s+=1;
+
+    printf("The sum is %lf.\n", s);
+
     return 0;
 }
