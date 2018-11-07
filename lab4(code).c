312b0cdb1aba3cca20654f6c504abb8656a4b12f
Nefito <43294012+Nefito@users.noreply.github.com>
2018-11-07 19:59:22 +0200

    Update lab4(code).c

--- lab4(code).c
+++ lab4(code).c
@@ -4,65 +4,77 @@
 
 int main()
 {
-    int i, j, k, l, min, counter;
+    int i, j, k, l, min, l1, counter;
     double n, s, L;
-    s = 0;
-    counter = 0;
-    int arr1[100];
-    double arr2[100];
+    s = 0, counter = 0;
+    int arr1[100], arr2[100];
+    double arr3[100];
 
     printf("Please enter the length of the array: ");
     scanf("%d", &l);
         if ( l > 50 || l < 0)
         {
-            do
+            do //here i check the proper input of our length
             {
                 printf("Enter a value greater than 0 and lesser than 50\n");
                 scanf("%d", &l);
             }while (l > 50 || l < 0);
         }
 
-
+    min = arr1[0];
     for (i = 0; i < l; i++)
     {
         arr1[i] = rand() % 10;
         printf("%d\n", arr1[i]);
-    }
-    printf("This array without its minimal values is:\n");
 
-    min = arr1[0]; //we find the minimal values of the array
-    for(i = 0; i < l; i++)
-    {
-        if(arr1[i] < min)
+        if(arr1[i] < min) //we find the minimal values of the array
         {
            min = arr1[i];
         }
     }
+    printf("This array without its minimal values is:\n");
+
+    l1 = l;
     for (i = 0; i < l; i++)
     {
-        if (arr1[i] == min) //here we destroy them
+        if (arr1[i] != min) //if the ith element isnt minimal we simply transfer it into the 2nd array
         {
-            arr1[i] = arr1[i+1];
-            arr1[i] = arr1[l-1];
-            l--;
+            arr2[i] = arr1[i];
         }
-        printf("%d\n", arr1[i]);
-        s += arr1[i];
+        else // but if it is we eleminate it
+        {
+            counter++;
+            if(arr1[i] == arr1[l-1])
+            {
+                arr2[i] = arr1[l-2];
+                l1--;
+            }
+            else
+            {
+                arr2[i] = arr1[l-counter];
+                l1--;
+            }
+        }
+    }
+    for(i = 0; i < l1; i++)
+    {
+        s += arr2[i];
+        printf("%d\n", arr2[i]);
     }
 
     printf("And the new array is:\n");
-    n = s / l;
-    L = l + 3;
+    n = s / l1;
+    L = l1 + 3;
     for (j = 0; j < 3; j++)
         {
-            arr2[j] = n;
-            printf("%.2f\n", arr2[j]);
+            arr3[j] = n;
+            printf("%.2f\n", arr3[j]);
         }
 
     for (i = 0, k = 3;k < L; i++, k++)
     {
-        arr2[k] = arr1[i];
-        printf("%.0f\n", arr2[k]);
+        arr3[k] = arr2[i];
+        printf("%.0f\n", arr3[k]);
     }
 
 return 0;
