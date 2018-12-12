61195fed6b982315b95cfeea1327e30e04b08523
Nefito <43294012+Nefito@users.noreply.github.com>
2018-12-13 01:01:13 +0200

    Update lab9(code).c

--- lab9(code).c
+++ lab9(code).c
@@ -1,37 +1,104 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int funct(int row, int column, int *a)
-{
-    int i, k, j, n, left, right, counter;
-    left = 0, right = 0, counter = 0;
+#define MAX 255
 
-    for(n = 1; n < column-2; n++) // n is the index of the column that splits the matrix
+void print_f(FILE *f)
+{
+     char ch;
+     ch = fgetc(f);
+     while(ch != EOF)
+     {
+        printf("%c", ch);
+        ch = fgetc(f);
+     }
+}
+
+int main()
+{
+    char cons[] = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z' };
+    int n, n1, n2, i, k, j, counter = 0, c_arr[MAX];
+    char arr[MAX];
+    FILE *f1;
+    FILE *f2;
+    f1 = fopen("f1.txt", "w+");
+    f2 = fopen("f2.txt", "w+");
+
+    if((f1 = fopen("f1.txt", "w+")) == NULL || (f2 = fopen("f2.txt", "w+")) == NULL)
     {
-        for(i = 0; i < row; i++)
+        printf("File could not be read.\n");
+        exit(0);
+    }
+
+    printf("Enter the amount of strings for f1: ");
+    scanf("%d", &n);
+
+    printf("Enter N1:\n");
+    scanf("%d", &n1);
+    if(n1 < 0 || n1 > n)
+    {
+        printf("enter a valid number!\n");
+        scanf("%d", &n1);
+    }
+    printf("Enter N2:\n");
+    scanf("%d", &n2);
+    if(n2 < 0 || n2 > n)
+    {
+        printf("enter a valid number!\n");
+        scanf("%d", &n2);
+    }
+
+    if(n1 > n2)
+    {
+        printf("N1 has to be lesser than N2!\n");
+        scanf("%d%d", &n1, &n2);
+    }
+
+    for( i = 0; i < n; i++)
+    {
+        printf("Enter string %d into the file:\n", i);
+        fgets(arr, MAX, stdin);
+
+        if(i > n1 && i < n2)
         {
-            for (k = 0; k < n; k++)
+            if(arr[0] == 'A')
             {
-                left+= *(a + i*n + k); //for every n i sum up the left half
+                fputs(arr, f2);
+                for(k = 0; k < strlen(arr); k++)
+                {
+                    for(j = 0; j < 19; j++)
+                    {
+                        if (arr[k] == cons[j])
+                        {
+                            counter++;
+                        }
+                    }
+                }
+                c_arr[i] = counter;
+                counter = 0;
             }
-            for(j = n+1; j < column - n; j++)
-            {
-                right+= *(a + i* column + j); //and the right half of the matrix
-            }
-            if(left > right)
-            {
-                counter++;
-            }
-            left = 0, right = 0; //reset left and right before n changes
+        }
+    }
+    int max = 0;
+    for(k = 0; k < i; k++)
+    {
+        if(c_arr[k] > max)
+        {
+            max = k;
         }
     }
 
-    if(counter >= 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    printf("F1:\n");
+    print_f(f1);
+
+    printf("F2:\n");
+    print_f(f2);
+
+    fclose(f1);
+    fclose(f2);
+
+    printf("The row in F2 which has the most consonants is row #%d!\n", max);
+
+    return 0;
 }
